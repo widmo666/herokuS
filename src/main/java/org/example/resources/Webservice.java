b797bf75626827e8545a28370e578ceb821b7298
Michał Wójtowicz <m.wojtowicz@tt.com.pl>
2015-09-07 12:33:03 +0200

    Prosty test

--- src/main/java/org/example/resources/Webservice.java
+++ src/main/java/org/example/resources/Webservice.java
@@ -1,8 +1,14 @@
 package org.example.resources;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 @Path("/web")
 public class Webservice {
@@ -12,6 +18,30 @@ public class Webservice {
 	@Produces("text/plain")
 	public String getMessage() {
 		return "Hello";
+	}
+
+	@Test
+	public void runTest() {
+		String t1 = webserwiceRunTest();
+		System.out.println("t1: " + t1);
+	}
+
+	@GET
+	@Path("/test")
+	@Produces("text/plain")
+	public String webserwiceRunTest() {
+		System.out.println("runTest()");
+
+		WebDriver webDriver = new FirefoxDriver();
+		webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		webDriver.navigate().to("https://wp.pl/");
+
+		String currentUrl = webDriver.getCurrentUrl();
+		System.out.println("currentUrl: " + currentUrl);
+
+		webDriver.close();
+		webDriver.quit();
+		return "Po testach";
 	}
 
 }
